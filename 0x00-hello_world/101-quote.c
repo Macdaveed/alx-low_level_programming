252b597879caf2cfc2f1b30c3ca62539ba07eb81
Macdaveed <Macdaveedgolden@gmail.com>
2023-09-07 22:59:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.>
 #include <unistd.h>
 /**
- * main - A program that prints a line to the standard error
+ * main - A c program that prints a line to the standard error
  * Return 1 (Success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return 1;
+	fputs("and that piece of useful\" - Dora korpar, 2015-10-15\n", stdout);
+	return (1);
 }
