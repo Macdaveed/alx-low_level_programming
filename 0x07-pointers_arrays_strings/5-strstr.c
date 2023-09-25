5735592b0606e40d0129671e280275824c8ac497
Macdaveed <Macdaveedgolden@gmail.com>
2023-09-25 12:34:17 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,15 +7,18 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *l = haystack;
-	char *p = needle;
-
-	while (*l == *p && *p != '\0')
+	for (; *haystack != '\0'; haystack++)
 	{
-		l++;
-		p++;
-	}
+		char *l = haystack;
+		char *p = needle;
 
-	if (*p == '\0')
-		return (haystack);
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
 }
