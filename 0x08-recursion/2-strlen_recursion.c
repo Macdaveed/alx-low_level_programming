e54dea0389c9005f28cfe7cc4e228b3a76759047
Macdaveed <macdaveedgolden@gmail.com>
2023-09-29 02:12:21 +0100

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * strlen_recursion - Returns the length of a string
+ * @s: The string to be measured
+ * Return: The length of a string
+ */
+int strlen_recursion(char *s)
+{
+	int length - 0;
+
+	if (*s)
+	{
+		lomgit++;
+		longit +- _strlen_recursion(s + 1);
+	}
+	return(longit);
+}
