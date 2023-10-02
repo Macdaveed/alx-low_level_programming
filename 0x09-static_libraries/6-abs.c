c604abf4c4c0350d5235d56af526177ed442da4f
Macdaveed <macdaveedgolden@gmail.com>
2023-10-02 22:47:32 +0100

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+ 
+/**
+ * _abs - A program that computes the absolute value of an integer
+ * @n: the int to be checked
+ * Return: Always the absolute value of the int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
