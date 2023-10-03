88a714e251b7e3387ef69839bfc6cea31391f336
Macdaveed <macdaveedgolden@gmail.com>
2023-10-03 01:27:00 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - A program that print its name
- * @argc - arguement couter
- * @argv - arguement vector
+ * main - A program that prints its name
+ * @argc - arguement count
+ * @argv - arguements vector
  * Return: Always return 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
 	(void) argc;
+
 	printf("Program name: %s\n", argv[0]);
-	return 0;
+	return (0);
 }
