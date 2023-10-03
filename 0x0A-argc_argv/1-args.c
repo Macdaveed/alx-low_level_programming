8919a5205f159aed6503e40a118d9b8f95ca2ca3
Macdaveed <macdaveedgolden@gmail.com>
2023-10-03 01:50:54 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints number of arguement
+ * @argc: argument counter
+ * @argv: arguments vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("the number of arguments: %d\n", argc);
+	return (0);
+}
