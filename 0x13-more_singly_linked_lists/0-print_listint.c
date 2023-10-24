fe4800b2f5c7eea08c8399fbf79e81642817a421
Macdaveed <macdaveedgolden@gmail.com>
2023-10-24 01:36:18 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,7 +9,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t node_Count = 0;
+	size_t node_count = 0;
 
 	while (h != NULL)
 	{
@@ -19,5 +19,3 @@ size_t print_listint(const listint_t *h)
 	}
 	return (node_count);
 }
-
-	
