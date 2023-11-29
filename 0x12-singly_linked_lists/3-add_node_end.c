44d4c615575eff069cfb0cff63066a154f022faa
Macdaveed <Macdaveedgolden@gmail.com>
2023-11-29 16:14:09 +0100

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,34 +11,26 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *current;
+	list_t *newnode, *tmp;
+	unsigned int i, count = 0;
 
-	if (str == NULL)
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
 		return (NULL);
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->str = strdup(str);
+	newnode->str = strdup(str);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	newnode->len = count;
+	newnode->next = NULL;
+	tmp = *head;
 
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = strlen(str);
-	new_node->next = NULL;
-	if (*head == NULL)
-	{
-		*head = new_node;
-	}
+	if (tmp == NULL)
+		*head = newnode;
 	else
 	{
-		current = *head;
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
-		current = current->next;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = newnode;
 	}
-	return (new_node);
+	return (*head);
 }
